26adaa1fc8f1cfe752391ea56591a08f71d2c083
Claire Kaufman <clairekm@umich.edu>
2026-02-02 15:54:48 -0500

    Initialize ABR1000_trial1.py
    Added initial setup for U235 material in OpenMC.

---
+++ ABR1000_trial1.py
@@ -0,0 +1,34 @@
+import openmc
+
+## Materials
+u235 = openmc.Material(name='U235')
+u235.add_nuclide('U235', 1.0)
+u235.set_density('g/cm3', 19.1)
+
+
+
+## Material mixtures
+
+## Materials file
+
+## Assembly Geometry
+
+## Inner assemblies
+
+## Outer assemblies
+
+## Primary Control
+
+## Secondary Control
+
+## Reflector
+
+## Shield
+
+## Core Geometry
+
+## Geometry file
+
+## Simulation parameters
+
+## EXECUTION
