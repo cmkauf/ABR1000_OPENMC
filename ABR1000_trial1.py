7549e592b1e835c06df7f1c70505e3f94e7ef9a1
Claire ('Cal') Kaufman <clairekm@umich.edu>
2026-02-09 15:40:34 -0500

    Update material densities and add new materials

--- ABR1000_trial1.py
+++ ABR1000_trial1.py
@@ -1,13 +1,81 @@
 import openmc
 
 ## Materials
+# 10 g/cm3 = placeholder value for density
 u235 = openmc.Material(name='U235')
 u235.add_nuclide('U235', 1.0)
-u235.set_density('g/cm3', 19.1)
+u235.set_density('g/cm3', 10.0)
 
+u238 = openmc.Material(name='U238')
+u238.add_nuclide('U238', 1.0)
+u238.set_density('g/cm3', 10.0)
 
+pu238 = openmc.Material(name='Pu238')
+pu238.add_nuclide('Pu238', 1.0)
+pu238.set_density('g/cm3', 10.0)
+
+pu239 = openmc.Material(name='U235')
+pu239.add_nuclide('Pu239', 1.0)
+pu239.set_density('g/cm3', 10.0)
+
+pu240 = openmc.Material(name='Pu240')
+pu240.add_nuclide('Pu240', 1.0)
+pu240.set_density('g/cm3', 10.0)
+
+pu241 = openmc.Material(name='Pu241')
+pu241.add_nuclide('Pu241', 1.0)
+pu241.set_density('g/cm3', 10.0)
+
+pu242 = openmc.Material(name='Pu242')
+pu242.add_nuclide('Pu242', 1.0)
+pu242.set_density('g/cm3', 10.0)
+
+am241 = openmc.Material(name='Am241')
+am241.add_nuclide('Am241', 1.0)
+am241.set_density('g/cm3', 10.0)
+
+o16 = openmc.Material(name='O16')
+o16.add_nuclide('O16', 1.0)
+o16.set_density('g/cm3', 10.0)
+
+sodium = openmc.Material(name='Na')
+sodium.add_nuclide('Na23', 1.0)
+sodium.set_density('g/cm3', 0.96)
+
+cu63 = openmc.Material(name='Cu63')
+cu63.set_density('g/cm3', 10.0)
+cu63.add_nuclide('Cu63', 1.0)
+
+Al2O3 = openmc.Material(name='Al2O3')
+Al2O3.set_density('g/cm3', 10.0)
+Al2O3.add_element('O', 3.0)
+Al2O3.add_element('Al', 2.0)
 
 ## Material mixtures
+inner = openmc.Material.mix_materials(
+  [],
+  [],
+  'wo')
+
+outer = openmc.Material.mix_materials(
+  [],
+  [],
+  'wo')
+
+cladding = openmc.Material.mix_materials(
+  [],
+  [],
+  'wo')
+
+shield = openmc.Material.mix_materials(
+  [],
+  [],
+  'wo')
+
+reflector = openmc.Material.mix_materials(
+  [],
+  [],
+  'wo')
 
 ## Materials file
 
